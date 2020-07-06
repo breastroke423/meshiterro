ee815b6e7d7222585c1ba7ed28b81b15699f953a
Mitsuhiro.Akeda <mitsuhiro.akeda@MitsuhiroAkedanoMacBook-Air.local>
2020-07-06 22:57:04 +0900

    0706end

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'dbf49c15b8d47bf35704cdb6178fa62780fb2add99cb607263bbd5c3176ea2b4810340039b5bf77a440a38376ec2b8220b0b0fc9aa36ee1778f3ef8240f0d905'
---
+++ config/initializers/kaminari_config.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+Kaminari.configure do |config|
+
+  config.default_per_page = 5
+  
+end
